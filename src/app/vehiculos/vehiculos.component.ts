5339d708549b79afb18692b9c83f3b4587479b22
daniela-mesa <d.mesam2@uniandes.edu.co>
2025-11-02 17:39:55 -0500

    Add vehiculos component

--- src/app/vehiculos/vehiculos.component.ts
+++ src/app/vehiculos/vehiculos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { VehiculosService } from './service/service.service';
+import { Vehiculo } from './models/vehiculo.model';
 
 @Component({
   selector: 'app-vehiculos',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehiculos.component.css']
 })
 export class VehiculosComponent implements OnInit {
+  vehiculos: Vehiculo[] = [];
+  marcaCount: { [marca: string]: number } = {}; 
 
-  constructor() { }
+  constructor(private vehiculosService: VehiculosService) {}
 
   ngOnInit() {
+    this.vehiculosService.getVehiculos().subscribe(data => {
+      this.vehiculos = data;
+      this.marcaCount = data.reduce((acc, vehiculo) => {
+        acc[vehiculo.marca] = (acc[vehiculo.marca] || 0) + 1;
+        return acc;
+      }, {} as { [marca: string]: number });
+    });
   }
-
 }
